48b54fc3c41a9066ad425ce7eaf0cd103a78f318
filmaj <maj.fil@gmail.com>
2023-08-29 22:51:32 -0400

    change to kanagawa nvim theme, add treesitter.

--- nvim/lua/configs/packagemanagement.lua
+++ nvim/lua/configs/packagemanagement.lua
@@ -13,24 +13,37 @@ vim.opt.rtp:prepend(lazypath)
 -- vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 
 require("lazy").setup({
+  { "rebelot/kanagawa.nvim", priority = 1000 },
+  { "nvim-lualine/lualine.nvim", priority = 999 },
   {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function ()
+      local configs = require("nvim-treesitter.configs")
+      configs.setup({
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "typescript", "bash", "css", "jsdoc", "json", "sql", "yaml"},
+        sync_install = false,
+        auto_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
+  },
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v2.x",
     dependencies = {
       -- LSP Support
-      { 'neovim/nvim-lspconfig' },             -- Required
-      { 'williamboman/mason.nvim' },           -- Optional
-      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+      { "neovim/nvim-lspconfig" },             -- Required
+      { "williamboman/mason.nvim" },           -- Optional
+      { "williamboman/mason-lspconfig.nvim" }, -- Optional
 
       -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },     -- Required
-      { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-      { 'L3MON4D3/LuaSnip' },     -- Required
+      { "hrsh7th/nvim-cmp" },     -- Required
+      { "hrsh7th/cmp-nvim-lsp" }, -- Required
+      { "L3MON4D3/LuaSnip" },     -- Required
     }
   },
-  'w0ng/vim-hybrid',
-  'vim-airline/vim-airline',
-  'vim-airline/vim-airline-themes',
-  'tpope/vim-sleuth',
-  'preservim/nerdcommenter',
+  "tpope/vim-sleuth",
+  "preservim/nerdcommenter",
 })
--- nvim/lua/configs/style.lua
+++ nvim/lua/configs/style.lua
@@ -1,8 +1,50 @@
+vim.o.termguicolors = true
 -- related to colors/hybrid.vim
-vim.cmd.colorscheme "hybrid"
 vim.o.background = "dark"
-vim.g.hybrid_custom_term_colors = 1
-vim.g.hybrid_reduced_contrast = 1
-vim.cmd.AirlineTheme "distinguished"
+require("kanagawa").setup({
+  background = { dark = "dragon", light = "lotus" },
+  colors = {
+    theme = {
+      all = {
+        ui = {
+          bg_gutter = "none"
+        },
+      },
+    },
+  },
+  commentStyle = { italic = false },
+  keywordStyle = { italic = false },
+  overrides = function(colors)
+    local theme = colors.theme
+    return {
+      -- dark completion popup menu
+      Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },  -- add `blend = vim.o.pumblend` to enable transparency
+      PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+      PmenuSbar = { bg = theme.ui.bg_m1 },
+      PmenuThumb = { bg = theme.ui.bg_p2 },
+      -- borderless info popup window
+      NormalFloat = { bg = "none" },
+      FloatBorder = { bg = "none" },
+      FloatTitle = { bg = "none" },
+
+      -- Save an hlgroup with dark background and dimmed foreground
+      -- so that you can use it where your still want darker windows.
+      -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+      NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+      -- Popular plugins that open floats will link to NormalFloat by default;
+      -- set their background accordingly if you wish to keep them dark and borderless
+      LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+      MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+    }
+end,
+})
+vim.cmd.colorscheme "kanagawa-dragon"
+require("lualine").setup({
+  options = {
+    icons_enabled = false,
+    theme = "iceberg",
+  },
+})
 -- Remove the background from float window
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
