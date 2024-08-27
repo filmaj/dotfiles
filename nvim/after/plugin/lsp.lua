ea7ab38c729a6a39fdc1eadc61d081677e389269
filmaj <maj.fil@gmail.com>
2024-08-27 16:43:00 -0400

    lsp tweaks

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -15,17 +15,19 @@ lsp.ensure_installed({
   "lua_ls",
   "tsserver",
 })
-config.biome.setup{}
+config.biome.setup {
+  root_dir = config.util.root_pattern("biome.json", "biome.jsonc"),
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format { async = false }
+      end,
+    })
+  end,
+}
 config.denols.setup {
   root_dir = config.util.root_pattern("deno.json", "deno.jsonc"),
-}
-config.golangci_lint_ls.setup{}
-config.gopls.setup{}
-config.jsonls.setup{}
-config.lua_ls.setup(lsp.nvim_lua_ls())
-config.tsserver.setup {
-  root_dir = config.util.root_pattern("package.json"),
-  single_file_support = false
 }
 config.eslint.setup({
   on_attach = function(client, bufnr)
@@ -35,6 +37,14 @@ config.eslint.setup({
     })
   end,
 })
+config.golangci_lint_ls.setup{}
+config.gopls.setup{}
+config.jsonls.setup{}
+config.lua_ls.setup(lsp.nvim_lua_ls())
+config.tsserver.setup {
+  root_dir = config.util.root_pattern("package.json"),
+  single_file_support = false
+}
 
 -- Text completions
 local cmp_mappings = lsp.defaults.cmp_mappings({
