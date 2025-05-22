818b0754388ccab95e9f913a8573f7089536c7f2
filmaj <maj.fil@gmail.com>
2025-05-22 11:14:33 -0400

    nvim: use latest telescope

--- nvim/lua/configs/packagemanagement.lua
+++ nvim/lua/configs/packagemanagement.lua
@@ -84,7 +84,7 @@ require("lazy").setup({
   },
   {
     "nvim-telescope/telescope.nvim",
-    tag = '0.1.8',
+    branch = "master",
     dependencies = { 'nvim-lua/plenary.nvim' },
   },
   { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
